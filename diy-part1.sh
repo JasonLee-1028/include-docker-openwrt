39d15cdc2b6dec9cd7af37b1c9d7dfce1a1f77bb
u know what <74245552+JasonLee-1028@users.noreply.github.com>
2024-06-19 21:37:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,18 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+#add environment
+sudo apt update -y
+sudo apt full-upgrade -y
+sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
+git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev \
+libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev \
+libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 \
+python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo \
+uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
